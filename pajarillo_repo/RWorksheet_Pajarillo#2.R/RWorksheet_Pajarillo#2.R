8660b2ce7a526783a62fc67b73c43ed7730dae08
Daisy-Ann-Pajarillo <daisyann.pajarillo@students.isatu.edu.ph>
2022-10-09 16:35:46 +0000

    added file

---
+++ pajarillo_repo/RWorksheet_Pajarillo#2.R/RWorksheet_Pajarillo#2.R
@@ -0,0 +1,124 @@
+#1.  Create a vector using : operator
+#A.  Sequence from -5 to 5. Write the R code and its output. 
+
+n <- seq(-5,5)
+n
+#Describe its output.
+# The output display from -5 up to 5
+
+
+# B. x<- 1:7. what will be the value of x?
+x <- 1:7
+x
+# The value of x is 1 2 3 4 5 6 7
+
+
+#2 create vector using seq()
+#A. seq(1, 3, by=0.2) # specify step size Write the R code and its output. 
+
+num <- seq(1, 3, by=0.2)
+num
+#Describe the output
+# The output jump or add by 0.2. This is the output[1] 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0, it start in number 1 and then jump or add by 0.2 until it reach the number 3 
+
+#3.A factory has a census of its workers. There are 50 workers in total. The following list shows their ages: 34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 43, 53, 41, 51, 35, 24,33, 41, 53, 40, 18, 44, 38, 41, 48, 27, 39, 19, 30, 61, 54, 58, 26, 18.
+
+worker <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27,
+            22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 43, 53, 41, 51, 35,
+            24,33, 41, 53, 40, 18, 44, 38, 41, 48, 27, 39, 19, 30, 61, 54, 58, 26,
+            18)
+worker
+#A  Access 3rd element, what is the value?
+num1 <- worker[3]
+num1
+#B  Access 2nd and 4th element, what are the values?
+num2 <- worker[2]
+num2
+
+num3 <- worker[4]
+num3
+
+#C. Access all but the 1st element is not included. Write the R code and its output.
+num4 <- worker[2:49]
+num4
+# Output. 
+#[1] 28 22 36 27 18 52 39 42 29 35 31 27 22 37 34 19 20 57 49 50 37 46 25 17 37 43 53 41 
+#[29] 51 35 24 33 41 53 40 18 44 38 41 48 27 39 19 30 61 54 58 26
+
+#4. *Create a vector x <- c("first"=3, "second"=0, "third"=9). Then named the vector, names(x).
+
+x <- c("first"=3, "second"=0, "third"=9)
+names(x)
+
+
+#A.  Print the results. Then access x[c("first", "third")]. Describe the output
+
+z <- x[c("first","third")]
+z
+
+#5. create sequence x from -3:2.
+
+#a. Modify 2nd element and change it to 0;
+#x[2] <- 0
+#x
+#Describe the output.
+
+x <- -3:2
+x[2] <- 0
+x
+
+#6. The following data shows the diesel fuel purchased by Mr. Cruz
+#Month Jan Feb March Apr May June
+#Price per liter (PhP) 52.50 57.25 60.00 65.00 74.25 54.00
+#Purchase–quantity(Liters) 25 30 40 50 10 45
+
+#A.Create a data frame for month, price per liter (php) and purchase-quantity (liter). Write the codes.
+
+month <- c("Jan", "Feb", "March", "Apr", "May", "June")
+month
+price <- c(52.50, 57.25, 60.00, 65.00, 74.25, 54.00)
+price
+purchase <- c(25, 30, 40, 50, 10, 45)  
+purchase
+
+#B.What is the average fuel expenditure of Mr. Cruz from Jan to June? Note: Use weighted.mean(liter, purchase)
+
+average <- weighted.mean(price, purchase)
+average
+
+#7 R has actually lots of built-in datasets. For example, the rivers data “gives the lengths (in miles) of 141 “major” rivers in North America, as compiled by the US Geological Survey”.
+
+#A. . Type “rivers” in your R console. Create a vector data with 7 elements, containing the number of elements (length) in rivers, their sum (sum), mean (mean), median (median), variance (var) standard deviation (sd), minimum (min) and maximum (max).
+
+data <- c(length(rivers), sum(rivers), mean(rivers), median(rivers), var(rivers),
+          sd(rivers), min(rivers), max(rivers))
+data
+
+#8. . The table below gives the 25 most powerful celebrities and their annual pay as ranked by the editions of Forbes magazine and as listed on the Forbes.com website.
+
+#A. Create vectors according to the above table. Write the codes.
+
+Power_Ranking <- 1:25
+Power_Ranking
+celebrity_name <- c("Tom Cruise", "Rolling Stones", "Oprah Winfrey", "U2", 
+                    "Tiger Woods", "Steven Spielberg", "Howard Stern", "50 Cent", "Cast of the sopranos",
+                    "Dan Brown", "Bruce Springsteen", "Donald Trump", "Muhammad Ali", "Paul McCartney",
+                    "George Lucas", "Elton John", "David Letterman", "Phil Mickelson", "J.K Rowling",
+                    "Bradd Pitt", "Peter Jackson", "Dr. Phil McGraw", "Jay Lenon", "Celine Dion", "Kobe Bryant")
+celebrity_name
+Pay <- c(67, 90, 225, 110, 90, 332, 302, 41, 52, 88, 55, 44, 55, 40,
+         233, 34, 40, 47, 75, 25, 39, 45, 32, 40, 31)
+Pay
+Data_Ranking <- data.frame(Power_Ranking, celebrity_name, Pay)
+Data_Ranking
+
+#B Modify the power ranking and pay of J.K. Rowling. Change power ranking to 15 and pay to 90. Write the codes and its output.
+
+Power_Ranking [19] <- 15
+Power_Ranking
+Pay [19] <- 90
+Pay
+
+ranking <- data.frame(Power_Ranking, celebrity_name, Pay)
+ranking
+
