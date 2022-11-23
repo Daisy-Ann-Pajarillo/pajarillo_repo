89a3d41e18c54ac992e9dbddbf60d0f4f5190ba6
Daisy-Ann-Pajarillo <daisyann.pajarillo@students.isatu.edu.ph>
2022-11-23 12:59:53 +0000

    added file

---
+++ pajarillo_repo/Rworksheet-3b-4-5/Rworksheet-4/Rworksheet-4.R
@@ -0,0 +1,93 @@
+#1. The table below shows the data about shoe size and height. Create a data frame..
+
+Shoesize <- c(6.5, 9.0, 8.5, 8.5, 10.5, 7.0, 9.5, 9.0, 13.0, 7.5, 10.5, 8.5, 12.0, 10.5, 13.0, 11.5, 8.5, 5.0, 10.0, 6.5, 7.5, 8.5, 10.5, 8.5, 10.5, 11.0, 9.0, 13.0)
+Shoesize
+
+Height <- c(66.0, 68.0, 64.5, 65.0, 70.0, 64.0, 70.0, 71.0, 72.0, 64.0, 74.5, 67.0, 71.0, 71.0, 77.0, 72.0, 59.0, 62.0, 72.0, 66.0, 64.0, 67.0, 73.0, 69.0, 72.0, 70.0, 69.0, 70.0)
+Height
+
+Gender <- c("F", "F", "F", "F", "M", "F", "F", "F", "M", "F", "M", "F", "M", "M", "M", "M", "F", "F", "M", "F", "F", "M", "M", "F", "M", "M", "M", "M")
+Gender
+
+
+
+data1 <- data.frame(Shoesize, Height, Gender)
+data1
+
+#a. Describe the data.
+  #Yes, there is a relationship that exists between shoe size and height. Short people have a smaller shoe size while taller people have a larger shoe size.
+
+
+#b. Find the mean of shoe size and height of the respondents. Copy the codes and results.
+
+respondent_shoesize <- mean(Shoesize)
+respondent_shoesize
+
+respondent_Height <- mean(Height)
+respondent_Height
+
+
+
+
+#c. Is there a relationship between shoe size and height? Why?
+  # Yes there are relationship betwen shoe size and height because people with large               shoe sizes are usually tall.
+
+
+
+
+#2. Construct character vector months to a factor with factor() and assign the result to factor_months_vector. Print out factor_months_vector and assert that R prints out the factor levels below the actual values.
+  #Consider data consisting of the names of months:
+  
+Month <- c("March","April","January","November","January",
+           "September","October","September","November","August",
+           "January","November","November","February","May","August",
+           "July","December","August","August","September","November","February","April"
+)
+Month
+
+
+factor_month_vector <- factor(Month)
+factor_month_vector
+
+
+#3. Then check the summary() of the months_vector and factor_months_vector. Interpret the results of both vectors. Are they both equally useful in this case?
+  
+  
+summary1 <-summary(Month)
+summary1
+
+summary2 <- summary(factor_month_vector)
+summary2
+
+#Are they both equally useful in this case?
+    #Yes because you can see how many data or months have been repeated
+
+
+#4. Create a vector and factor for the table below.
+
+a <- c("East"= '1', "West"= '4', "North" = '3')
+a
+
+new_order_data <- factor(a,levels = c("East","West","North"))
+print(new_order_data)
+
+
+
+
+
+#5. Enter the data below in Excel with file name = import_march.csv
+#a. Import the excel file into the Environment Pane using read.table() function.
+#Write the code
+
+getwd()
+import_data <- read.table("/cloud/project/pajarillo_repo/Rworksheet-3b-4-5/Rworksheet-4/import_march.csv", header = TRUE, sep=",")
+import_data
+
+#b. View the dataset. Write the code and its result
+
+
+
+daisy <- read.csv("import_march.csv")
+View(daisy)
+
+
